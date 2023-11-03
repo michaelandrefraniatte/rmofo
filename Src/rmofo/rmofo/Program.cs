302731473d6b2a845e33a888454f329b4b28ead6
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-11-03 18:12:03 +0100

    Fix a bug

--- Src/rmofo/rmofo/Program.cs
+++ Src/rmofo/rmofo/Program.cs
@@ -15,15 +15,29 @@ namespace rmofo
             string rootPath = System.Windows.Forms.Application.StartupPath; 
             Console.WriteLine(rootPath);
             ProtectFiles(rootPath);
-            ProtectDirs(rootPath);
+            ProtectDir(rootPath); 
             Console.WriteLine("done");
+            string[] fileNames = Directory.GetFiles(rootPath);
+            foreach (string fileName in fileNames)
+            {
+                Console.WriteLine(fileName);
+                ProtectFile(fileName);
+                Console.WriteLine("done");
+            }
             string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
             foreach (string dir in dirs)
             {
                 Console.WriteLine(dir);
                 ProtectFiles(dir);
-                ProtectDirs(dir);
+                ProtectDir(dir);
                 Console.WriteLine("done");
+                string[] files = Directory.GetFiles(dir);
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                    ProtectFile(file);
+                    Console.WriteLine("done");
+                }
             }
         }
         public static void ProtectFiles(string dirName)
@@ -43,7 +57,7 @@ namespace rmofo
             var process = Process.Start(start);
             process.WaitForExit();
         }
-        public static void ProtectDirs(string dirName)
+        public static void ProtectDir(string dirName)
         {
             if (!Directory.Exists(dirName))
             {
@@ -54,6 +68,23 @@ namespace rmofo
                 FileName = "powershell.exe",
                 RedirectStandardOutput = true,
                 Arguments = $"attrib +S '{dirName}' /d",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            var process = Process.Start(start);
+            process.WaitForExit();
+        }
+        public static void ProtectFile(string name)
+        {
+            if (!File.Exists(name))
+            {
+                return;
+            }
+            var start = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                RedirectStandardOutput = true,
+                Arguments = $"attrib +S '{name}'",
                 CreateNoWindow = true,
                 UseShellExecute = false
             };
